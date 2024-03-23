f769162f77ac1e0ed6357c369c1c559f609c57c4
knighthat <knightchanelgaming@gmail.com>
2024-03-22 22:37:59 -0500

    Create routing & 404 page for unknown paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './assets/css/global.css'
+import Err404 from "./template/404/404.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {HomePage} from "./template/homepage/homepage.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='*' element={<Err404/>}/>
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>
 )
